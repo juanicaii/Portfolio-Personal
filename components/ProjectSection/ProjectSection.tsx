0ed5efd15c440957d41f38d852b12b78681a015a
juaniseijas <juaniseijas00@gmail.com>
2021-01-12 05:51:55 -0300

    projects section

--- components/ProjectSection/ProjectSection.tsx
+++ components/ProjectSection/ProjectSection.tsx
@@ -1,11 +1,127 @@
 import styles from './ProjectSection.module.css'
 import TitleSection from '../TitleSection'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 interface IProjectSection {
   section: string
 }
+interface IProject {
+  title: string
+  desc: string
+  tecnology: any[]
+}
+
+const Project = ({ title, desc, tecnology }: IProject) => {
+  const [isHover, setIsHover] = useState(false)
+  return (
+    <motion.div
+      onHoverStart={() => {
+        setIsHover(true)
+      }}
+      onHoverEnd={() => {
+        setIsHover(false)
+      }}
+      className={styles.project}
+    >
+      <div className={styles.back}>
+        <div className={styles.desc}>
+          <span>Amaip</span>
+          <p>Diseño, Frontend y Backend</p>
+          <p>Tecnologias utilizadas</p>
+          <div className={styles.tec}>
+            {tecnology.map((tecn) => (
+              <p>{tecn.tec}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+      <motion.div
+        animate={isHover ? { translateX: 350 } : { translateX: 0 }}
+        className={styles.front}
+        transition={{ duration: 0.6 }}
+      >
+        <img width="300" src="/projects/img-1.jpg" alt="" />
+      </motion.div>
+    </motion.div>
+  )
+}
 
 const ProjectSection = ({ section }: IProjectSection) => {
+  const projects = [
+    {
+      title: 'Amaip',
+      desc: 'Diseño, Frontend y Backend',
+      tecnology: [
+        { tec: 'HTML' },
+        { tec: 'JS' },
+        { tec: 'PHP' },
+        { tec: 'CSS' },
+        { tec: 'JQUERY' },
+        { tec: 'MYSQL' },
+      ],
+    },
+    {
+      title: 'Amaip',
+      desc: 'Diseño, Frontend y Backend',
+      tecnology: [
+        { tec: 'HTML' },
+        { tec: 'JS' },
+        { tec: 'PHP' },
+        { tec: 'CSS' },
+        { tec: 'JQUERY' },
+        { tec: 'MYSQL' },
+      ],
+    },
+    {
+      title: 'Amaip',
+      desc: 'Diseño, Frontend y Backend',
+      tecnology: [
+        { tec: 'HTML' },
+        { tec: 'JS' },
+        { tec: 'PHP' },
+        { tec: 'CSS' },
+        { tec: 'JQUERY' },
+        { tec: 'MYSQL' },
+      ],
+    },
+    {
+      title: 'Amaip',
+      desc: 'Diseño, Frontend y Backend',
+      tecnology: [
+        { tec: 'HTML' },
+        { tec: 'JS' },
+        { tec: 'PHP' },
+        { tec: 'CSS' },
+        { tec: 'JQUERY' },
+        { tec: 'MYSQL' },
+      ],
+    },
+    {
+      title: 'Amaip',
+      desc: 'Diseño, Frontend y Backend',
+      tecnology: [
+        { tec: 'HTML' },
+        { tec: 'JS' },
+        { tec: 'PHP' },
+        { tec: 'CSS' },
+        { tec: 'JQUERY' },
+        { tec: 'MYSQL' },
+      ],
+    },
+    {
+      title: 'Amaip',
+      desc: 'Diseño, Frontend y Backend',
+      tecnology: [
+        { tec: 'HTML' },
+        { tec: 'JS' },
+        { tec: 'PHP' },
+        { tec: 'CSS' },
+        { tec: 'JQUERY' },
+        { tec: 'MYSQL' },
+      ],
+    },
+  ]
   return (
     <div>
       <TitleSection
@@ -14,6 +130,11 @@ const ProjectSection = ({ section }: IProjectSection) => {
         description=" Lorem Ipsum is simply dummy text of the printing and typesetting industry Ipsum has been
           the industry"
       />
+      <div className={styles.container}>
+        {projects.map((project) => (
+          <Project title={project.title} desc={project.desc} tecnology={project.tecnology} />
+        ))}
+      </div>
     </div>
   )
 }
